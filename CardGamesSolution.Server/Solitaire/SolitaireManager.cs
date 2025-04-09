688d0018c6cf068e95155b5d0c3e3546635d42fa
gwilcoxen5 <gwilcoxen5@gmail.com>
2025-04-09 12:11:36 -0500

    Update SolitaireManager.cs

--- CardGamesSolution.Server/Solitaire/SolitaireManager.cs
+++ CardGamesSolution.Server/Solitaire/SolitaireManager.cs
@@ -1,80 +1,57 @@
-﻿using CardGamesSolution.Server.Shared;
+using System;
+using System.Collections.Generic;
 
-namespace CardGamesSolution.Server.Solitaire
-{
-    public class SolitaireManager : ISolitaireManager
-    {
+namespace SolitaireEngine {
+    public class SolitaireManager : ISolitaireManager {
         private ISolitaireEngine engine;
-        private List<Card> deck;
-        // filled deck
+        private Deck deck;
 
-        public SolitaireManager(ISolitaireEngine solitaireEngine)
-        {
+        public SolitaireManager(ISolitaireEngine solitaireEngine) {
             engine = solitaireEngine;
-            deck = GenerateDeck();
-
-            // constructor, new deck of cards
+            deck = new Deck();
         }
 
-        public void StartNewGame()
-        {
-            Shuffle(deck);
-            engine.InitializeGame(new List<Card>(deck));
+        public void StartNewGame() {
+            deck.Shuffle();
+            engine.InitializeGame(deck.GetCards());
 
-            // starts new game shuffles deck and deals cards
+            // method for starting or restarting game
         }
 
-        public void DealCards()
-        {
-            // we can use this if needed to be seperated when using UI, for animations 
+        public void DealCards() {
+            // Placeholder
         }
 
-        public void Move(Card card, string fromPile, string toPile)
-        {
+        public void Move(Card card, string fromPile, string toPile) {
             var result = engine.ProcessMove(card, fromPile, toPile);
             Console.WriteLine(result.Message);
 
-            // checks if the move a player trys to do is valid, prints to console
+            // calls engine to move card, engine checks if valid, result is printed to console
         }
 
-        public void Restart()
-        {
+        public void DrawFromStock() {
+            var gameState = engine.GetGameState();
+            if (gameState.Stock.Count > 0) {
+                var card = gameState.Stock[0];
+                gameState.Stock.RemoveAt(0);
+                gameState.Waste.Add(card);
+            } else {
+                Console.WriteLine("Stock is empty.");
+            }
+
+            // if there are cards left in stock, move top one to the waste, if stock is empty, print that it is
+        }
+
+        public void Restart() {
             StartNewGame();
+
+            // calls StartNewGame again to restart
         }
 
-        public GameState GetGameState()
-        {
+        public GameState GetGameState() {
             return engine.GetGameState();
 
-            // gets current state, tableau, foundation, stock etc.
-        }
-
-        private List<Card> GenerateDeck()
-        {
-            string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
-            string[] ranks = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
-
-            List<Card> fullDeck = new();
-
-            foreach (var suit in suits)
-                foreach (var rank in ranks)
-                    fullDeck.Add(new Card(suit, rank)); // adds each deck combo to deck
-
-            return fullDeck;
-
-            // creates and returns a 52 card deck, 
-        }
-
-        private void Shuffle(List<Card> cards)
-        {
-            Random rand = new Random();
-            for (int i = cards.Count - 1; i > 0; i--)
-            {
-                int j = rand.Next(i + 1);
-                (cards[i], cards[j]) = (cards[j], cards[i]);
-
-                // randomly shuffles the deck of cards
-            }
+            // gets gamestate
         }
     }
 }
