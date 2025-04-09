6e0f9084c3943d5f3f1288603a095dd1cb2e6871
gwilcoxen5 <gwilcoxen5@gmail.com>
2025-04-09 12:09:16 -0500

    Update GameState.cs

--- CardGamesSolution.Server/Solitaire/GameState.cs
+++ CardGamesSolution.Server/Solitaire/GameState.cs
@@ -1,24 +1,37 @@
-﻿using CardGamesSolution.Server.Shared;
+using System.Collections.Generic;
 
-namespace CardGamesSolution.Server.Solitaire
-{
-    public class GameState
-    {
-        public List<List<Card>> Tableau { get; set; }
-        public List<Card> Foundation { get; set; }
+namespace SolitaireEngine {
+    public class GameState {
+        public List<TableauPile> Tableau { get; set; }
+        public Dictionary<string, List<Card>> Foundation { get; set; }
         public List<Card> Stock { get; set; }
         public List<Card> Waste { get; set; }
         public int Score { get; set; }
 
-        public GameState()
-        {
-            Tableau = new List<List<Card>>();
-            for (int i = 0; i < 7; i++) Tableau.Add(new List<Card>());
+        // holds all live game data
 
-            Foundation = new List<Card>();
+        public GameState() {
+            Tableau = new List<TableauPile>();
+            for (int i = 0; i < 7; i++) {
+                Tableau.Add(new TableauPile());
+
+                // initializes 7 tableau columns each as empty Pile
+            }
+
+            Foundation = new Dictionary<string, List<Card>> {
+                { "Hearts", new List<Card>() },
+                { "Diamonds", new List<Card>() },
+                { "Clubs", new List<Card>() },
+                { "Spades", new List<Card>() }
+
+                // sets up four foundation piles, 1 per suit
+            };
+
             Stock = new List<Card>();
             Waste = new List<Card>();
             Score = 0;
+
+            // Initializes stock waste and score if we want to add score
         }
     }
 }
