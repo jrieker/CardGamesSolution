ff10c7d3c9b028d508475b5122f35bcf788d8390
gwilcoxen5 <gwilcoxen5@gmail.com>
2025-04-09 12:10:04 -0500

    Update ISolitaireAccessor.cs

--- CardGamesSolution.Server/Solitaire/ISolitaireAccessor.cs
+++ CardGamesSolution.Server/Solitaire/ISolitaireAccessor.cs
@@ -1,13 +1,12 @@
-﻿using CardGamesSolution.Server.Shared;
+using System.Collections.Generic;
 
-namespace CardGamesSolution.Server.Solitaire
-{
-    public interface ISolitaireAccessor : ICardGamesAccessor
-    {
+namespace SolitaireEngine {
+    public interface ISolitaireAccessor {
         List<Card> GetCards();
-        Dictionary<string, string> GetCardImages(); // You can replace with Image later
+        Dictionary<string, string> GetCardImages();
         string LoadRules();
         void SaveGame(GameState gameState);
         GameState LoadSavedGame();
     }
+    // none of this is really implemented yet
 }
