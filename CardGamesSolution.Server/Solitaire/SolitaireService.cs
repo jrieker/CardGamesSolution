1ee33d075bb38f9c677a75e4176ed99f49dcaf7b
gwilcoxen5 <gwilcoxen5@gmail.com>
2025-04-11 15:15:02 -0500

    Create SolitaireService.cs

---
+++ CardGamesSolution.Server/Solitaire/SolitaireService.cs
@@ -0,0 +1,27 @@
+namespace CardGamesSolution.Server.UserAccount {
+    public class SolitaireService {
+        private readonly ISolitaireManager manager;
+        private readonly ISolitaireEngine engine;
+
+        public SolitaireService() {
+            engine = new SolitaireEngine.SolitaireEngine();
+            manager = new SolitaireManager(engine);
+        }
+
+        public void StartGame() {
+            manager.StartNewGame();
+        }
+
+        public GameState GetState() {
+            return engine.GetGameState();
+        }
+
+        public MoveResult MoveCard(Card card, string from, string to) {
+            return engine.ProcessMove(card, from, to);
+        }
+
+        public void DrawFromStock() {
+            manager.DrawFromStock();
+        }
+    }
+}
