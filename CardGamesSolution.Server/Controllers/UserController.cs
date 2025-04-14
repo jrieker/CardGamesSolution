5ce45baa1e8c28d3682cee9b5658e4bb8a8b951a
jrieker <78100915+jrieker@users.noreply.github.com>
2025-04-14 08:41:20 -0500

    User Controller

---
+++ CardGamesSolution.Server/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CardGamesSolution.Server.UserAccount;
+using CardGamesSolution.Server.Database;
+
+namespace CardGamesSolution.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly LoginManager _loginManager;
+
+        public UserController(LoginManager loginManager)
+        {
+            _loginManager = loginManager;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserDto user)
+        {
+            try
+            {
+                User loggedInUser = _loginManager.Login(user.Username, user.Password);
+                return Ok(new { success = true, username = loggedInUser.Username });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { success = false, message = ex.Message });
+            }
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] UserDto user)
+        {
+            try
+            {
+                _loginManager.Register(user.Username, user.Password);
+                return Ok(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { success = false, message = ex.Message });
+            }
+        }
+    }
+
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
