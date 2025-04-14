4eae0db2ab7dd0625dac5c10e9b5243426715d09
jrieker <78100915+jrieker@users.noreply.github.com>
2025-04-14 09:35:41 -0500

    Add files via upload

---
+++ CardGamesSolution.Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using CardGamesSolution.Server.UserAccount;
+using CardGamesSolution.Server.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<LoginManager>();
+builder.Services.AddScoped<IUserDataAccessor, UserDataAccessor>();
+builder.Services.AddScoped<IDatabaseConnection, DatabaseConnection>();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
