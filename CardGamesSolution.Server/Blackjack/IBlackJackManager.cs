bd177df9bcb54e35d56f2bba0229923b1630d8cb
Jacob Rieker <78100915+jrieker@users.noreply.github.com>
2025-05-09 20:25:34 -0500

    Update IBlackJackManager.cs

--- CardGamesSolution.Server/Blackjack/IBlackJackManager.cs
+++ CardGamesSolution.Server/Blackjack/IBlackJackManager.cs
@@ -1,14 +1,18 @@
-﻿using CardGamesSolution.Server.Shared;
+using CardGamesSolution.Server.Shared;
 using CardGamesSolution.Server.UserAccount;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CardGamesSolution.Server.Blackjack
 {
     public interface IBlackJackManager
     {
-        MultiplayerGameState InitializeGame(User[] users);
-        ActionResultDto PlaceBet(BetCommandDto cmd);
-        ActionResultDto Hit(HitCommandDto cmd);
-        ActionResultDto Stand(StandCommandDto cmd);
-        MultiplayerGameState GetState();
+        IActionResult Start([FromBody] User[] users);
+        IActionResult Deal();
+        IActionResult Hit([FromBody] int userId);
+        IActionResult Stand([FromBody] int userId);
+        IActionResult Double([FromBody] int userId);
+        IActionResult Bet([FromBody] BetRequestDto data);
+        IActionResult DealerStep();
+        IActionResult EndRound();
     }
 }
