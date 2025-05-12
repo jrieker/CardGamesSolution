3b0f5c02f5afdc627d6ab7cc79ed1386c74a3ca9
Jacob Rieker <78100915+jrieker@users.noreply.github.com>
2025-05-11 23:06:45 -0500

    Add files via upload

---
+++ CardGamesSolution.Server/UserAccount/ILoginManager.cs
@@ -0,0 +1,8 @@
+﻿namespace CardGamesSolution.Server.UserAccount
+{
+    public interface ILoginManager
+    {
+        User Login(string username, string password);
+        void Register(string username, string password);
+    }
+}
---
+++ CardGamesSolution.Server/UserAccount/LoginManager.cs
@@ -0,0 +1,44 @@
+﻿namespace CardGamesSolution.Server.UserAccount
+{
+    public class LoginManager : ILoginManager
+    {
+        private readonly IUserDataAccessor userDataAccessor;
+
+        public LoginManager(IUserDataAccessor userDataAccessor)
+        {
+            this.userDataAccessor = userDataAccessor;
+        }
+
+        public User Login(string username, string password)
+        {
+            if (userDataAccessor.UserExists(username))
+            {
+                var user = userDataAccessor.GetUserByUsername(username);
+                if (user.Password == password)
+                {
+                    return user;
+                }
+                throw new Exception("Incorrect password!");
+            }
+            throw new Exception("Username not found!");
+        }
+
+        public void Register(string username, string password)
+        {
+            if (userDataAccessor.UserExists(username))
+                throw new Exception("User already exists!");
+
+            int userId = userDataAccessor.GetNextUserId();
+            float startingBalance = 500;
+            var user = new User(userId, username, password, 0, 0, startingBalance, 0);
+            userDataAccessor.SaveUserData(user);
+        }
+    }
+
+public class UserDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
+
